7ff897f8d161f05efa4b13ee90d0bb9c8907d56e
Qiutong Men <qm2017@nyu.edu>
2023-07-10 20:44:19 +0000

    populate storage framework.

--- envoy/storage/storage.h
+++ envoy/storage/storage.h
@@ -2,9 +2,10 @@
 // Created by qiutong on 7/10/23.
 //
 
-/**
- * Low level storage management interface, to be used by state replication module.
- */
+#include <string>
+
+#include "envoy/common/pure.h"
+#include "envoy/buffer/buffer.h"
 
 #pragma once
 
@@ -13,10 +14,42 @@ namespace States {
 
 class StorageMetadata {
   // service, pod, module, session, version
+  // ttl
+  // recover handshake port, uri.
+
+  // need design choice: how to implement incremental (delta) storage?
 };
 
-class Storage {
+/**
+ * Low level distributed storage management interface, to be used by state replication module.
+ */
 
+class Storage {
+public:
+  virtual void write(StorageMetadata& metadata, Buffer::Instance& obj //, some bytes array, or object
+  ) PURE;
+
+  virtual void replicate(const std::string& resource_id) PURE;
+
+  // Not supporting packed transmission, just for calling convenience.
+  virtual void replicate(std::vector<const std::string &> resource_ids) PURE;
+
+  // TODO: consider if we can pack up this.
+  virtual void replicateSvc(const std::string& service_id) PURE;
+
+  virtual void recover(const std::string& resource_id) PURE;
+
+  // Not supporting packed transmission, just for calling convenience.
+  virtual void recover(std::vector<const std::string &> resource_ids) PURE;
+
+  // TODO: consider if we can pack up this.
+  virtual void recoverSvc(const std::string& service_id) PURE;
+
+  virtual void deactivate(const std::string& resource_id) PURE;
+
+  virtual void deactivate(std::vector<const std::string &> resource_ids) PURE;
+
+  virtual void deactivateSvc(const std::string& service_id) PURE;
 };
 
 } // namespace States
