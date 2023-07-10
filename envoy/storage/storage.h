0e41998505c67f28d81f1ef822095102822bd43d
Qiutong Men <qm2017@nyu.edu>
2023-07-10 19:55:17 +0000

    Fix interface of LdsApi.

---
+++ envoy/storage/storage.h
@@ -0,0 +1,21 @@
+//
+// Created by qiutong on 7/10/23.
+//
+
+/**
+ * Low level storage management interface, to be used by state replication module.
+ */
+
+#pragma once
+
+namespace Envoy {
+namespace States {
+
+class Storage {
+
+};
+
+} // namespace States
+} // namespace Envoy
+
+
