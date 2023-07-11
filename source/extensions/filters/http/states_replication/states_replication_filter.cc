9897a04de3a14da0547061e4a4d8f024de01a6b5
Qiutong Men <qm2017@nyu.edu>
2023-07-11 19:50:05 +0000

    init stateReplicationFilter framework.

---
+++ source/extensions/filters/http/states_replication/states_replication_filter.cc
@@ -0,0 +1,18 @@
+//
+// Created by qiutong on 7/11/23.
+//
+
+#include "source/extensions/filters/http/states_replication/states_replication_filter.h"
+
+
+namespace Envoy {
+namespace Extensions {
+namespace HttpFilters {
+namespace States {
+
+
+
+}
+}
+}
+}
---
+++ source/extensions/filters/http/states_replication/states_replication_filter.h
@@ -0,0 +1,21 @@
+//
+// Created by qiutong on 7/11/23.
+//
+
+#pragma once
+
+#include "source/extensions/filters/http/common/pass_through_filter.h"
+
+namespace Envoy {
+namespace Extensions {
+namespace HttpFilters {
+namespace States {
+
+class StatesReplicationFilter : public Http::PassThroughDecoderFilter {
+  
+};
+
+}
+}
+}
+}
