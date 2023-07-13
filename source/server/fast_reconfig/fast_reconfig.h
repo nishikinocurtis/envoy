3b3238475968d60b050e998e4cf16d7ae142a992
Ruochen Miao <rm5327@nyu.edu>
2023-07-13 15:53:24 +0800

    complete virtual functions

--- source/server/fast_reconfig/fast_reconfig.h
+++ source/server/fast_reconfig/fast_reconfig.h
@@ -5,19 +5,20 @@
 #pragma once
 
 #include "envoy/network/filter.h"
+#include "envoy/network/listen_socket.h"
 #include "envoy/server/fast_reconfig.h"
 #include "envoy/server/instance.h"
 #include "envoy/server/listener_manager.h"
-#include "envoy/network/listen_socket.h"
-#include "source/common/http/header_map_impl.h"
+#include "source/common/buffer/buffer_impl.h"
+#include "source/common/common/empty_string.h"
 #include "source/common/http/conn_manager_config.h"
 #include "source/common/http/conn_manager_impl.h"
-#include "source/common/buffer/buffer_impl.h"
+#include "source/common/http/header_map_impl.h"
+#include "source/server/fast_reconfig/fast_reconfig_filter.h"
 #include "source/server/fast_reconfig/listener_handler.h"
+#include "source/server/null_overload_manager.h"
 #include "source/server/server_endpoint_listener.h"
 #include "source/server/server_endpoint_filter.h"
-#include "source/server/fast_reconfig/fast_reconfig_filter.h"
-#include "source/server/null_overload_manager.h"
 #include "source/server/server_endpoint_socket_factory.h"
 
 #include "source/extensions/listener_managers/listener_manager/lds_api.h"
@@ -45,6 +46,9 @@ public:
   class GrpcMessageImpl;
 
   using GenRequestFn = std::function<GrpcRequestProcessorPtr(AdminStream&)>;
+
+  using HttpConnectionManagerProto =
+      envoy::extensions::filters::network::http_connection_manager::v3::HttpConnectionManager;
 
   class GrpcRequestProcessorImpl : public GrpcRequestProcessor {
   public:
@@ -109,6 +113,103 @@ public:
                                const Network::Socket::OptionsSharedPtr& socket_options,
                                Stats::ScopeSharedPtr&& listener_scope);
 
+  // Http::ConnectionManagerConfig
+  const Http::RequestIDExtensionSharedPtr& requestIDExtension() override {
+    return request_id_extension_;
+  }
+  const std::list<AccessLog::InstanceSharedPtr>& accessLogs() override { return access_logs_; }
+  const absl::optional<std::chrono::milliseconds>& accessLogFlushInterval() override {
+    return null_access_log_flush_interval_;
+  }
+  bool flushAccessLogOnNewRequest() override { return flush_access_log_on_new_request_; }
+  Http::ServerConnectionPtr createCodec(Network::Connection& connection,
+                                        const Buffer::Instance& data,
+                                        Http::ServerConnectionCallbacks& callbacks) override;
+  Http::DateProvider& dateProvider() override { return date_provider_; }
+  std::chrono::milliseconds drainTimeout() const override { return std::chrono::milliseconds(100); }
+  Http::FilterChainFactory& filterFactory() override { return *this; }
+  bool generateRequestId() const override { return false; }
+  bool preserveExternalRequestId() const override { return false; }
+  bool alwaysSetRequestIdInResponse() const override { return false; }
+  absl::optional<std::chrono::milliseconds> idleTimeout() const override { return idle_timeout_; }
+  bool isRoutable() const override { return false; }
+  absl::optional<std::chrono::milliseconds> maxConnectionDuration() const override {
+    return max_connection_duration_;
+  }
+  uint32_t maxRequestHeadersKb() const override { return max_request_headers_kb_; }
+  uint32_t maxRequestHeadersCount() const override { return max_request_headers_count_; }
+  std::chrono::milliseconds streamIdleTimeout() const override { return {}; }
+  std::chrono::milliseconds requestTimeout() const override { return {}; }
+  std::chrono::milliseconds requestHeadersTimeout() const override { return {}; }
+  std::chrono::milliseconds delayedCloseTimeout() const override { return {}; }
+  absl::optional<std::chrono::milliseconds> maxStreamDuration() const override {
+    return max_stream_duration_;
+  }
+  Router::RouteConfigProvider* routeConfigProvider() override { return &route_config_provider_; }
+  Config::ConfigProvider* scopedRouteConfigProvider() override {
+    return &scoped_route_config_provider_;
+  }
+  const std::string& serverName() const override { return Http::DefaultServerString::get(); }
+  const absl::optional<std::string>& schemeToSet() const override { return scheme_; }
+  HttpConnectionManagerProto::ServerHeaderTransformation
+  serverHeaderTransformation() const override {
+    return HttpConnectionManagerProto::OVERWRITE;
+  }
+  Http::ConnectionManagerStats& stats() override { return stats_; }
+  Http::ConnectionManagerTracingStats& tracingStats() override { return tracing_stats_; }
+  bool useRemoteAddress() const override { return true; }
+  const Http::InternalAddressConfig& internalAddressConfig() const override {
+    return internal_address_config_;
+  }
+  uint32_t xffNumTrustedHops() const override { return 0; }
+  bool skipXffAppend() const override { return false; }
+  const std::string& via() const override { return EMPTY_STRING; }
+  Http::ForwardClientCertType forwardClientCert() const override {
+    return Http::ForwardClientCertType::Sanitize;
+  }
+  const std::vector<Http::ClientCertDetailsType>& setCurrentClientCertDetails() const override {
+    return set_current_client_cert_details_;
+  }
+  const Network::Address::Instance& localAddress() override;
+  const absl::optional<std::string>& userAgent() override { return user_agent_; }
+  Tracing::HttpTracerSharedPtr tracer() override { return nullptr; }
+  const Http::TracingConnectionManagerConfig* tracingConfig() override { return nullptr; }
+  Http::ConnectionManagerListenerStats& listenerStats() override { return listener_->stats_; }
+  bool proxy100Continue() const override { return false; }
+  bool streamErrorOnInvalidHttpMessaging() const override { return false; }
+  const Http::Http1Settings& http1Settings() const override { return http1_settings_; }
+  bool shouldNormalizePath() const override { return true; }
+  bool shouldMergeSlashes() const override { return true; }
+  bool shouldStripTrailingHostDot() const override { return false; }
+  Http::StripPortType stripPortType() const override { return Http::StripPortType::None; }
+  envoy::config::core::v3::HttpProtocolOptions::HeadersWithUnderscoresAction
+  headersWithUnderscoresAction() const override {
+    return envoy::config::core::v3::HttpProtocolOptions::ALLOW;
+  }
+  const LocalReply::LocalReply& localReply() const override { return *local_reply_; }
+  envoy::extensions::filters::network::http_connection_manager::v3::HttpConnectionManager::
+      PathWithEscapedSlashesAction
+      pathWithEscapedSlashesAction() const override {
+    return envoy::extensions::filters::network::http_connection_manager::v3::HttpConnectionManager::
+        KEEP_UNCHANGED;
+  }
+  const std::vector<Http::OriginalIPDetectionSharedPtr>&
+  originalIpDetectionExtensions() const override {
+    return detection_extensions_;
+  }
+  const std::vector<Http::EarlyHeaderMutationPtr>& earlyHeaderMutationExtensions() const override {
+    return early_header_mutations_;
+  }
+  uint64_t maxRequestsPerConnection() const override { return 0; }
+  const HttpConnectionManagerProto::ProxyStatusConfig* proxyStatusConfig() const override {
+    return proxy_status_config_.get();
+  }
+  Http::HeaderValidatorPtr makeHeaderValidator(Http::Protocol) override {
+    return nullptr;
+  }
+  bool appendXForwardedPort() const override { return false; }
+  bool addProxyProtocolConnectionState() const override { return true; }
+
 private:
 
 
@@ -156,6 +257,28 @@ private:
   std::vector<Network::ListenSocketFactoryPtr> socket_factories_;
   std::unique_ptr<ReconfigListener> listener_;
   bool ignore_global_conn_limit_;
+  Http::RequestIDExtensionSharedPtr request_id_extension_;
+  std::list<AccessLog::InstanceSharedPtr> access_logs_;
+  const absl::optional<std::chrono::milliseconds> null_access_log_flush_interval_;
+  const bool flush_access_log_on_new_request_ = false;
+  Http::SlowDateProviderImpl date_provider_;
+  absl::optional<std::chrono::milliseconds> idle_timeout_;
+  absl::optional<std::chrono::milliseconds> max_connection_duration_;
+  absl::optional<std::chrono::milliseconds> max_stream_duration_;
+  NullRouteConfigProvider route_config_provider_;
+  NullScopedRouteConfigProvider scoped_route_config_provider_;
+  const absl::optional<std::string> scheme_{};
+  Http::ConnectionManagerStats stats_;
+  Http::ConnectionManagerTracingStats tracing_stats_;
+  const AdminInternalAddressConfig internal_address_config_;
+  std::vector<Http::ClientCertDetailsType> set_current_client_cert_details_;
+  absl::optional<std::string> user_agent_;
+  ReconfigListenerPtr listener_;
+  Http::Http1Settings http1_settings_;
+  const LocalReply::LocalReplyPtr local_reply_;
+  const std::vector<Http::OriginalIPDetectionSharedPtr> detection_extensions_{};
+  const std::vector<Http::EarlyHeaderMutationPtr> early_header_mutations_{};
+  std::unique_ptr<HttpConnectionManagerProto::ProxyStatusConfig> proxy_status_config_;
 
 };
 
