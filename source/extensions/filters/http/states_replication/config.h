f29a270a8af74bb6943121ecea7fe083401b2ad8
Qiutong Men <qm2017@nyu.edu>
2023-07-25 22:51:57 +0000

    upd: adding framework for registering StatesReplicationFilter

--- source/extensions/filters/http/states_replication/config.h
+++ source/extensions/filters/http/states_replication/config.h
@@ -4,13 +4,17 @@
 
 #pragma once
 
+#include "source/extensions/filters/http/common/factory_base.h"
+
 namespace Envoy {
 namespace Extensions {
 namespace HttpFilters {
 namespace States {
 
-class StatesReplicationFilterConfig {
-  
+class StatesReplicationFilterFactory {
+  // it seems we really don't need some specific config
+  // just inherits from FactoryBase and register the filter
+  // declare a simple .proto in api/envoy/extensions/...
 };
 
 }
